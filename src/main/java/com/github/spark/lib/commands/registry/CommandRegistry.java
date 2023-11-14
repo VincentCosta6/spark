25c98589d2844c2d192f451f854d1f24ca9fe891
Vincent Costa <costa.vincent132@gmail.com>
2023-11-13 23:39:49 -0800

    import-cleanup (#30)

--- src/main/java/com/github/spark/lib/commands/registry/CommandRegistry.java
+++ src/main/java/com/github/spark/lib/commands/registry/CommandRegistry.java
@@ -1,14 +1,10 @@
 package com.github.spark.lib.commands.registry;
 
 import com.github.spark.lib.commands.CommandNode;
-import com.github.spark.lib.commands.registry.CommandReflection;
 import com.github.spark.lib.common.Registry;
 import com.github.spark.lib.framework.Framework;
-import com.github.spark.lib.framework.FrameworkInjectable;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CommandRegistry extends Registry<String, CommandNode> {
 
--- src/main/java/com/github/spark/lib/framework/FrameworkInjectable.java
+++
@@ -1,5 +0,0 @@
-package com.github.spark.lib.framework;
-
-public interface FrameworkInjectable {
-    public boolean injectMembers();
-}
