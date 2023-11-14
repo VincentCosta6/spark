bfc30b9ad7d85cf9a899fb03bee670fa61a99b0e
Vincent Costa <costa.vincent132@gmail.com>
2023-11-13 17:29:03 -0800

    update version, add service to example

--- src/main/java/com/github/spark/examples/commands/shop/ShopAdminCommands.java
+++ src/main/java/com/github/spark/examples/commands/shop/ShopAdminCommands.java
@@ -1,11 +1,16 @@
 package com.github.spark.examples.commands.shop;
 
+import com.github.spark.examples.services.CustomService;
 import com.github.spark.lib.commands.Command;
 import com.github.spark.lib.commands.CommandHandler;
 import com.github.spark.lib.commands.dto.CommandContext;
+import com.google.inject.Inject;
 
 @CommandHandler(name = "admin")
 public class ShopAdminCommands implements Command {
+    @Inject
+    CustomService customService;
+
     public boolean onCommand(CommandContext context) {
         context.playerCommandEvent().player().sendMessage("admin command!");
         return false;
@@ -13,6 +18,7 @@ public class ShopAdminCommands implements Command {
 
     @CommandHandler(name = "add-item", description = "usage: add-item [item-id] [amount]")
     public boolean onBuyCommand(CommandContext context) {
+        customService.printTestAndPlayerStateCount();
         context.playerCommandEvent().player().sendMessage("add item command!");
         return false;
     }
